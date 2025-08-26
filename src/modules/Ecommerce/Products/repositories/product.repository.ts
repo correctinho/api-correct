9bfca858812dfb4d910dee026a812a89c716a29b
jamessonseren <jamesson_seren@hotmail.com>
2025-08-26 12:59:59 -0400

    insert product history

--- src/modules/Ecommerce/Products/repositories/product.repository.ts
+++ src/modules/Ecommerce/Products/repositories/product.repository.ts
@@ -4,7 +4,7 @@ import { ProductEntity } from '../entities/product.entity';
 
 export interface IProductRepository extends RepositoryInterface<ProductEntity> {
    createProduct(entity: ProductEntity): Promise<ProductEntity>
-   upsert(entity: ProductEntity): Promise<ProductEntity>;
+   //upsert(entity: ProductEntity): Promise<ProductEntity>;
    findBusinessProducts(businessInfoUuid: string): Promise<ProductEntity[] | []>;
    findActiveProductsByBusinessId(businessInfoUuid: string): Promise<ProductEntity[] | []>;
    delete(entity: ProductEntity): Promise<void>;
